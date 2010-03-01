0c27659b57c29996e2a8465e5daba2b5d266415f
Nathan Kelley <nathan@devsolo.com>
2010-02-28 19:13:59 -0500

    Finished with 2nd attempt

--- v02/src/swe101-GreedScorer/GreedScorerTests.cs
+++ v02/src/swe101-GreedScorer/GreedScorerTests.cs
@@ -63,27 +63,161 @@ namespace swe101_GreedScorer
         #endregion
 
         [TestMethod]
-        public void Should_Return_100_When_For_A_Single_1()
+        public void Should_Return_100_When_Roll_Is_A_Single_1()
         {
             gs.CalculateScore(1).ShouldEqual(100);
         }
 
         [TestMethod]
-        public void Should_Return_200_When_For_Two_1s()
+        public void Should_Return_200_When_Roll_Is_Two_1s()
         {
-            gs.CalculateScore(1,1).ShouldEqual(200);
+            gs.CalculateScore(1, 1).ShouldEqual(200);
+        }
+
+        [TestMethod]
+        public void Should_Return_1000_When_Roll_Is_Three_1s()
+        {
+            gs.CalculateScore(1, 1, 1).ShouldEqual(1000);
+        }
+
+        [TestMethod]
+        public void Should_Return_1100_When_Roll_Is_Four_1s()
+        {
+            gs.CalculateScore(1, 1, 1, 1).ShouldEqual(1100);
+        }
+
+        [TestMethod]
+        public void Should_Return_1200_When_Roll_Is_Five_1s()
+        {
+            gs.CalculateScore(1, 1, 1, 1, 1).ShouldEqual(1200);
+        }
+
+        [TestMethod]
+        public void Should_Return_2000_When_Roll_Is_Six_1s()
+        {
+            gs.CalculateScore(1, 1, 1, 1, 1, 1).ShouldEqual(2000);
+        }
+
+        [TestMethod]
+        public void Should_Return_50_When_Roll_Is_One_5()
+        {
+            gs.CalculateScore(5).ShouldEqual(50);
+        }
+
+        [TestMethod]
+        public void Should_Return_100_When_Roll_Is_Two_5s()
+        {
+            gs.CalculateScore(5, 5).ShouldEqual(100);
+        }
+
+        [TestMethod]
+        public void Should_Return_500_When_Roll_Is_Three_5s()
+        {
+            gs.CalculateScore(5, 5, 5).ShouldEqual(500);
+        }
+
+        [TestMethod]
+        public void Should_Return_550_When_Roll_Is_Four_5s()
+        {
+            gs.CalculateScore(5, 5, 5, 5).ShouldEqual(550);
+        }
+
+        [TestMethod]
+        public void Should_Return_600_When_Roll_Is_Five_5s()
+        {
+            gs.CalculateScore(5, 5, 5, 5, 5).ShouldEqual(600);
+        }
+
+        [TestMethod]
+        public void Should_Return_1000_When_Roll_Is_Six_5s()
+        {
+            gs.CalculateScore(5, 5, 5, 5, 5, 5).ShouldEqual(1000);
+        }
+
+        [TestMethod]
+        public void Should_Return_0_When_Roll_Is_Any_Single_Number_Other_Than_1_Or_5()
+        {
+            gs.CalculateScore(2).ShouldEqual(0);
+            gs.CalculateScore(3).ShouldEqual(0);
+            gs.CalculateScore(4).ShouldEqual(0);
+            gs.CalculateScore(6).ShouldEqual(0);
+        }
+
+        [TestMethod]
+        public void Should_Return_0_When_Roll_Is_Any_Double_Number_Other_Than_1_Or_5()
+        {
+            gs.CalculateScore(2, 2).ShouldEqual(0);
+            gs.CalculateScore(3, 3).ShouldEqual(0);
+            gs.CalculateScore(4, 4).ShouldEqual(0);
+            gs.CalculateScore(6, 6).ShouldEqual(0);
+        }
+
+        [TestMethod]
+        public void Should_Return_200_When_Roll_Is_Three_2s()
+        {
+            gs.CalculateScore(2, 2, 2).ShouldEqual(200);
+            gs.CalculateScore(2, 2, 2, 2, 2, 2).ShouldEqual(400);
+        }
+
+        [TestMethod]
+        public void Should_Return_300_When_Roll_Is_Three_3s()
+        {
+            gs.CalculateScore(3, 3, 3).ShouldEqual(300);
+            gs.CalculateScore(3, 3, 3, 3, 3, 3).ShouldEqual(600);
+        }
+
+        [TestMethod]
+        public void Should_Return_400_When_Roll_Is_Three_4s()
+        {
+            gs.CalculateScore(4, 4, 4).ShouldEqual(400);
+            gs.CalculateScore(4, 4, 4, 4, 4, 4).ShouldEqual(800);
+        }
+
+        [TestMethod]
+        public void Should_Return_600_When_Roll_Is_Three_6s()
+        {
+            gs.CalculateScore(6, 6, 6).ShouldEqual(600);
+            gs.CalculateScore(6, 6, 6, 6, 6, 6).ShouldEqual(1200);
+        }
+
+        [TestMethod]
+        public void Should_Return_Appropriate_Result_For_Acceptance_Tests()
+        {
+            gs.CalculateScore(1, 1, 1, 5, 1).ShouldEqual(1150);
+            gs.CalculateScore(2, 3, 4, 6, 2).ShouldEqual(0);
+            gs.CalculateScore(3, 4, 5, 3, 3).ShouldEqual(350);
+            gs.CalculateScore(1, 5, 1, 2, 4).ShouldEqual(250);
         }
     }
 }
 
 public class GreedScorer
 {
-    public GreedScorer()
-    {
-    }
-
     public int CalculateScore(params int[] Rolls)
     {
-        return Rolls.Where(x=>x==1).Sum() * 100;
+        int score = 0;
+        score += Rolls.CountTriples(1) * 1000;
+        score += Rolls.CountSingles(1) * 100;
+        score += Rolls.CountTriples(2) * 200;
+        score += Rolls.CountTriples(3) * 300;
+        score += Rolls.CountTriples(4) * 400;
+        score += Rolls.CountTriples(5) * 500;
+        score += Rolls.CountSingles(5) * 50;
+        score += Rolls.CountTriples(6) * 600;
+        return score;
     }
 }
+
+public static class GreedScorerExtensions
+{
+    public static int CountTriples(this int[] Rolls, int NumberToLookFor)
+    {
+        return Rolls.Where(x => x == NumberToLookFor).Count() / 3;
+    }
+
+    public static int CountSingles(this int[] Rolls, int NumberToLookFor)
+    {
+        return Rolls.Where(x => x == NumberToLookFor).Count() % 3;
+    }
+}
+
